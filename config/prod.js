2655868b01f18cd6d208f42432781229261f9ae7
yom-elect <adepitanoriyomi11@yahoo.com>
2020-02-13 17:55:51 +0100

    Google private keys

--- config/prod.js
+++ config/prod.js
@@ -2,7 +2,7 @@ const config = {
   googleProjectID: process.env.GOOGLE_PROJECT_ID,
   dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
   googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-  googlePrivateKey: process.env.GOOGLE_PRIVATE_KEYS
+  googlePrivateKey: JSON.parse(process.env.GOOGLE_PRIVATE_KEYS)
 };
 
 module.exports = config;
